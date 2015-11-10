b94ae9b694b1f96f7450a8ded6f64c0aae8d9f2b
Shawn Aten <shawnmaten@gmail.com>
2015-11-10 14:55:09 -0600

    Add modules/APIUser.js.

---
+++ modules/APIUser.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var APIUserSchema = new Schema({
+  id: Number,
+  email: String,
+  password: String
+});
+
+var conn = mongoose.createConnection('mongodb://mongodb/api');
+conn.on('error', console.error.bind(console, 'connection error:'));
+conn.once('open', function (callback) {
+
+});
+
+module.exports = conn.model('User', APIUserSchema);
