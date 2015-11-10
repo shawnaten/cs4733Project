26d3a4fcc68520c6b4ae40417875688908d6afb8
Shawn Aten <shawnmaten@gmail.com>
2015-11-10 14:58:35 -0600

    Implement /account/create/ endpoint.

--- routes/account.js
+++ routes/account.js
@@ -1,14 +1,41 @@
-var assert = require('assert');
+var APIUser = require('../modules/APIUser');
 var BankUser = require('../modules/BankUser');
 var express = require('express');
 var router = express.Router();
 
 // Create account
-router.get('/create', function(req, res, next) {
-  BankUser.find(function (err, bankUsers) {
+router.post(/create\/\d+/, function(req, res, next) {
+  var email = req.query.email;
+  var id = req.path.match(/create\/(\d+)/)[1];
+  var name = req.query.name;
+  var ssn = req.query.ssn;
+
+  var bankQueryCallback = function (err, existingUser) {
     if (err) return console.error(err);
-    res.send({ meta: { code: 501 }, data: bankUsers });
-  });
+    if (existingUser) {
+      var apiQuery = APIUser.findOne({ id: id });
+      apiQuery.exec(apiQueryCallback);
+    }
+    else
+      res.send({ meta: { code: 404, cause: 'user not found', } });
+  };
+
+  var apiQueryCallback = function (err, existingUser) {
+    if (existingUser)
+      res.send({ meta: { code: 404, cause: 'account exists', } });
+    else {
+      var user = new APIUser({ email: email, id: id, password: 'password', });
+      user.save(function (err, apiUser) {
+        if (err) return console.error(err);
+        res.send({ meta: { code: 200 } });
+        // code to send email
+      });
+    }
+  };
+
+  var bankQuery = BankUser.findOne({ id: id, name: name, ssn: ssn });
+  bankQuery.exec(bankQueryCallback);
+
 });
 
 // Change password
