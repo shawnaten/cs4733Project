05cf9506ce874b0b6339bbfd4fbbfe8c89b51278
Shawn Aten <shawnmaten@gmail.com>
2015-11-17 11:33:12 -0600

    Change to generate token and send email.

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,10 @@
 var APIUser = require('../modules/APIUser');
 var BankUser = require('../modules/BankUser');
+var bcrypt = require('bcrypt');
+var emailTransporter = require('../modules/emailTransporter');
+var nodemailer = require('nodemailer');
 var express = require('express');
+var randomToken = require('random-token');
 var router = express.Router();
 
 // Create account
@@ -10,37 +14,59 @@ router.post(/create\/\d+/, function(req, res, next) {
   var name = req.query.name;
   var ssn = req.query.ssn;
 
-  var bankQueryCallback = function (err, existingUser) {
-    if (err) return console.error(err);
+  var bankQuery = BankUser.findOne({ id: id, name: name, ssn: ssn });
+  bankQuery.exec(bankQueryCallback);
+
+  function bankQueryCallback(err, existingUser) {
+    if (err) return console.err(err);
     if (existingUser) {
       var apiQuery = APIUser.findOne({ id: id });
       apiQuery.exec(apiQueryCallback);
     }
     else
       res.send({ meta: { code: 404, cause: 'user not found', } });
-  };
+  }
 
-  var apiQueryCallback = function (err, existingUser) {
+  function apiQueryCallback(err, existingUser) {
     if (existingUser)
       res.send({ meta: { code: 404, cause: 'account exists', } });
     else {
-      var user = new APIUser({ email: email, id: id, password: 'password', });
-      user.save(function (err, apiUser) {
-        if (err) return console.error(err);
-        res.send({ meta: { code: 200 } });
-        // code to send email
+      var token = randomToken(16);
+      var hash = bcrypt.hashSync(token, 10);
+      var user = new APIUser({ email: email, id: id, password: hash, });
+
+      var mailOptions = {
+        to: name+" <"+email+">",
+        from: "CS4773 Node-API "+"<utsa.cs.4733.restful.node@gmail.com>",
+        subject: "Email Confirmation",
+        text: "🔑 == " + token,
+      };
+
+      emailTransporter.sendMail(mailOptions, function(err, info) {
+        if (err)return console.log(err);
+        user.save(function (err, apiUser) {
+          if (err) return console.log(err);
+          res.send({ meta: { code: 200 }, });
+        });
       });
     }
-  };
-
-  var bankQuery = BankUser.findOne({ id: id, name: name, ssn: ssn });
-  bankQuery.exec(bankQueryCallback);
+  }
 
 });
 
 // Change password
-router.get('/password', function(req, res, next) {
-  res.send({ meta : { code : 501 } });
+router.get('/password/*', function(req, res, next) {
+
+  var apiQuery = APIUser.findOne({ id: id });
+  apiQuery.exec(apiQueryCallback);
+
+  function apiQueryCallback(err, existingUser) {
+    if (!existingUser) {
+      res.send({ meta: { code: 404, cause: 'login ivalid', } });
+    } else {
+
+    }
+  }
 });
 
 // Change email
