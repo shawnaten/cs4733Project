ddb021cd9f582841dc5b37bd51a590084e31ad8d
Shawn Aten <shawnmaten@gmail.com>
2015-11-17 11:32:17 -0600

    Add nodemailer transporter for Gmail.

---
+++ modules/emailTransporter.js
@@ -0,0 +1,9 @@
+var nodemailer = require('nodemailer');
+
+module.exports = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: "utsa.cs.4733.restful.node@gmail.com",
+    pass: process.env.EMAIL_PASS
+  }
+});
